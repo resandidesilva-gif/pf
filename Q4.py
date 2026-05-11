bc37e0ac91f293663d1ecb3a908adb168c7019b6
resandidesilva-gif <resandidesilva@gmail.com>
2026-05-11 17:44:18 +0530

    Q4

---
+++ Q4.py
@@ -0,0 +1,6 @@
+length=10
+width=5
+area=length*width
+print("The area of a rectangle with length" +  str(length) +" and width "+ str(width) + " is " +str(area))
+      
+The area of a rectangle with length10 and width 5 is 50
